87ef68d46c618a35ab20b0448974e9c8e59485e5
Tyler Tompa <noreply>
2018-09-10 22:10:27 -0400

    Calculated total votes for each candidate

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -13,9 +13,28 @@ column_names = ["voter_id", "county", "candidate"]
 votes = pandas.read_csv(csv_election_data, names=column_names, low_memory=False)
 candidates= votes.candidate.tolist()
 
+votes_correy = 0
+votes_khan = 0
+votes_li = 0
+votes_otooley = 0
+for i in range(len(candidates)):
+    if candidates[i] == "Correy":
+        votes_correy += 1
+    if candidates[i] == "Khan":
+        votes_khan += 1
+    elif candidates[i] == "Li":
+        votes_li += 1
+    elif candidates[i] == "O'Tooley":
+        votes_otooley += 1
+
 # for i in candidates:
 #     print(candidates[i])
 
 print("Election Results")
 print("-------------------------")
 print(f"Total Votes: {len(candidates)-1}")
+print("-------------------------")
+print(f"Khan: {votes_khan}")
+print(f"Correy: {votes_correy}")
+print(f"Li: {votes_li}")
+print(f"O'Tooley: {votes_otooley}")
