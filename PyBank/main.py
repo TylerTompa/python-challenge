4e60adad82464909c7094bed03c2a93939ae45ae
Tyler Tompa <noreply>
2018-09-17 16:49:29 -0400

    Rewrote a few comments

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,4 +1,4 @@
-# The os package allows us to create files paths across systems.
+# The os package allows us to create files paths across different operating systems.
 # The csv package allows us to read and write to csv files.
 import os
 import csv
@@ -6,23 +6,24 @@ import csv
 # This creates a file path leading to the budget_data csv file so that users on different operating systems can use this file.
 csv_budget_data = os.path.join("Resources", "budget_data.csv")
 
-# Later we will use a for loop to create a list of profits and months using the columns from our csv file.  This is to initialize those lists so that we may use the append() method.
+# Later we will use a for-loop to create a list of profits and months using the columns from our csv file.  This is to initialize those lists so that we may use the append() method.
 months = []
 profits = []
 
-# This reads the budget_data csv file and uses a for loop to create lists composed of each column.
+# This reads the budget_data csv file and uses a for loop to create lists composed of the contents from each column.
 with open(csv_budget_data,"r") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     for row in csvreader:
         months.append(row[0])
         profits.append(row[1])
 
+""" We will create a variable called total_profit, then we will use a for-loop to add the profit from every month, which will result in the total profit across every month.  We must ensure that we do not include the first row, as this is a heading and thus a string object."""
 total_profit = 0
 for i in range(len(profits)):
     if i != 0:
         total_profit += int(profits[i])
 
-# The variable changes is a list which stores the change in profits for each month.  The suceeding for-loop will find the difference between each pair of adjancent months and store them in this list.  We will then be able to use primitive functions to find the sum and length of this list, and thereafter the average.
+""" The variable changes is a list which stores the change in profits for each month.  The suceeding for-loop will find the difference between each pair of adjancent months and store them in this list.  We will then be able to use primitive functions to find the sum and length of this list, and thereafter the average.  Again we must ensure not to include the first row, as this is a column heading and thus a string object."""
 changes = []
 for i in range(len(profits)):
     if i not in (0,1):
@@ -31,7 +32,7 @@ for i in range(len(profits)):
 
 average_change = round(sum(changes)/len(changes),2)
 
-# We must add 2 to correctly offset the month list to match the changes between months, which will be explain now: The first change is found by subtracting the first row in profits from the second row.  The first month can not have a change, as it is the initial value, and thus has nothing to subtract from it.  We therefore must add 1 to our month index to offset this difference; that is, the first change happens in the second month, the second change in the third month, etc.  We must also add 1 to our index again since the first row is the heading "Date," which puts the first month in the second row, the second month in the third row, etc.
+""" We must add 2 to correctly offset the month list to match the changes between months, which will be explained now: The first change is found by subtracting the first row in profits from the second row.  The first month can not have a change, as it is the initial value, and thus has nothing to subtract from it.  We therefore must add 1 to our month index to offset this difference; that is, the first change happens in the second month, the second change in the third month, etc.  We must also add 1 to our index again since the first row is the heading "Date," which puts the first month in the second row, the second month in the third row, etc.""" 
 greatest_profit_increase_index = changes.index(max(changes))
 offset_greatest_increase = greatest_profit_increase_index + 2
 
@@ -47,7 +48,7 @@ print(f"Average Change: ${average_change}")
 print(f"Greatest Increase in Profits: {months[offset_greatest_increase]} (${max(changes):,})")
 print(f"Greatest Decrease in Profits: {months[offset_greatest_decrease]} (${min(changes):,})")
 
-# This creates a path which a text file with the analysis will be exported to.
+# This creates a file path which a text file with the analysis will be exported to.
 text_export = os.path.join("financial_analysis.txt")
 
 # This creates a text file with the analysis and exports it to the path defined above.
@@ -56,8 +57,8 @@ with open(text_export, "w", newline="") as txt_file:
 
     csv_writer.writerow(["Financial Analysis"])
     csv_writer.writerow(["----------------------------"])
-    csv_writer.writerow([f"Total months: {len(months)-1}"])
-    csv_writer.writerow([f"Total profits: ${total_profit}"])
-    csv_writer.writerow([f"Average Change: ${average_change}"])
+    csv_writer.writerow([f"Total months: {len(months)-1:,}"])
+    csv_writer.writerow([f"Total profits: ${total_profit:,}"])
+    csv_writer.writerow([f"Average Change: ${average_change:,}"])
     csv_writer.writerow([f"Greatest Increase in Profits: {months[offset_greatest_increase]} (${max(changes):,})"])
     csv_writer.writerow([f"Greatest Decrease in Profits: {months[offset_greatest_decrease]} (${min(changes):,})"])
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,22 +1,21 @@
-# This os package allows us to create file paths across systems.
+# This os package allows us to create file paths across different operating systems.
 # The csv package allows us to read and write csv files.
-# The mode function from the statistics package will allow us to determine which candidate appears most frequently in the list of candidates.
 import os
 import csv
 
 # This creates a file path leading to the election_data csv file so that users on different operating systems can use this file.
 csv_election_data = os.path.join("Resources","election_data.csv")
 
-# Later we will use a for loop to count the number of votes each candidate recieved by counting the number of times each candidate's name appears on a list.  This is to initialize that list.
+# Later we will use a for-loop to count the number of votes each candidate recieved by counting the number of times each candidate's name appears on a list.  This is to initialize that list.
 candidates = []
 
-# This reads the election_data csv file and uses a for loop to create a list of votes by appending a candidate's name to the list each time it is found in the csv file.
+# This reads the election_data csv file and uses a for-loop to create a list of votes by appending a candidate's name to the list each time it is found in the csv file.
 with open(csv_election_data, "r") as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ",")
     for row in csvreader:
         candidates.append(row[2]) 
 
-# This loop iterates through out list of candidates and adds one vote each time the candidate's name is found.
+# This loop iterates through our list of candidates and adds one vote each time the candidate's name is found.
 votes_correy = 0
 votes_khan = 0
 votes_li = 0
@@ -31,20 +30,22 @@ for i in range(len(candidates)):
     elif candidates[i] == "O'Tooley":
         votes_otooley += 1
 
-# This creates a list, and stores therein the percent of votes each candiate recieves.  We divide the the number of votes for each candidate by one less than the length of the list, since the first row of the list is a header. 
+# This creates a list, and stores therein the percent of votes each candiate recieves.  We divide the the number of votes for each candidate by one less than the length of the list, since the first row of the list is a header.
 vote_percentages = []
 
-percent_votes_correy = round((100*(votes_correy/(len(candidates)-1))),3)
-percent_votes_khan = round((100*(votes_khan/(len(candidates)-1))),3)
-percent_votes_li = round((100*(votes_li/(len(candidates)-1))),3)
-percent_votes_otooley = round((100*(votes_otooley/(len(candidates)-1))),3)
+percent_votes_correy = round((100*(votes_correy/(len(candidates)-1))),2)
+percent_votes_khan = round((100*(votes_khan/(len(candidates)-1))),2)
+percent_votes_li = round((100*(votes_li/(len(candidates)-1))),2)
+percent_votes_otooley = round((100*(votes_otooley/(len(candidates)-1))),2)
 
 vote_percentages.append(percent_votes_correy)
 vote_percentages.append(percent_votes_khan)
 vote_percentages.append(percent_votes_li)
 vote_percentages.append(percent_votes_otooley)
 
-"""This sets the winner to Correy, then checks to see if Khan has more votes than Correy.  If so, the winner is set to Khan.  It then checks to see if Li has more votes than Correy and more votes than Khan; if so, the winner is set to Li.  Finally it checks to see if O'Tooley has more votes than Correy, and if O'Tooley has more votes than Khan, and if O'Tooley has more votes than Li, if so, the winner is set to O'Tooley.  By checking multiple conditions with the keyword "and," we ensure that each candidate must have more votes than every candidate that came before.  It is not necessary to check multiple conditions on each line, as Khan is only considered if he has more votes than Coorey, and if Khan has fewer votes than another candidate, then Khan will be replaced as the winner.  The same logic applies for each successive candidate."""
+"""This sets the winner to Correy, then checks to see whether each successive candidate has a higher percentage of votes than each candidate that came before.  If Khan has more votes than Correy, then the winner is set to Khan.  It then checks to see whether Li has more votes than Correy and more votes than Khan; if so, the winner is set to Li.  Finally it checks to see whether O'Tooley has more votes than Correy, and whether O'Tooley has more votes than Khan, and whether O'Tooley has more votes than Li; if so, the winner is set to O'Tooley.  By checking multiple conditions with the keyword "and," we ensure that each candidate must have more votes than every candidate that came before.
+
+It is not necessary to check multiple conditions on each line.  Khan- for example- is only considered if he has more votes than the one candidate that came before him- Correy, and will surely be replaced if a succeeding candiate has a higher percentage of votes than himself."""
 winner = "Correy"
 if percent_votes_khan > percent_votes_correy:
     winner = "Khan"
@@ -57,17 +58,17 @@ if (percent_votes_otooley > percent_votes_correy) and (percent_votes_otooley > p
 # This prints the results.
 print("Election Results")
 print("-------------------------")
-print(f"Total Votes: {len(candidates)-1}")
+print(f"Total Votes: {len(candidates)-1:,}")
 print("-------------------------")
-print(f"Khan: {percent_votes_khan}% ({votes_khan})")
-print(f"Correy: {percent_votes_correy}% ({votes_correy})")
-print(f"Li: {percent_votes_li}% ({votes_li})")
-print(f"O'Tooley: {percent_votes_otooley}% ({votes_otooley})")
+print(f"Khan: {percent_votes_khan}% ({votes_khan:,})")
+print(f"Correy: {percent_votes_correy}% ({votes_correy:,})")
+print(f"Li: {percent_votes_li}% ({votes_li:,})")
+print(f"O'Tooley: {percent_votes_otooley}% ({votes_otooley:,})")
 print("-------------------------")
 print(f"Winner: {winner}")
 print("-------------------------")
 
-# This creates a path which a text file with the results wll be exported to.
+# This creates a file path which a text file with the results wll be exported to.
 text_export = os.path.join("election_results.txt")
 
 # This creates a text file with the results and exports it to the path defined above.
@@ -76,12 +77,12 @@ with open(text_export, "w",newline="") as txt_file:
 
     csv_writer.writerow(["Election Results"])
     csv_writer.writerow(["-------------------------"])
-    csv_writer.writerow([f"Total Votes: {len(candidates)-1}"])
+    csv_writer.writerow([f"Total Votes: {len(candidates)-1:,}"])
     csv_writer.writerow(["-------------------------"])
-    csv_writer.writerow([f"Khan: {percent_votes_khan}% ({votes_khan})"])
-    csv_writer.writerow([f"Correy: {percent_votes_correy}% ({votes_correy})"])
-    csv_writer.writerow([f"Li: {percent_votes_li}% ({votes_li})"])
-    csv_writer.writerow([f"O'Tooley: {percent_votes_otooley}% ({votes_otooley})"])
+    csv_writer.writerow([f"Khan: {percent_votes_khan}% ({votes_khan:,})"])
+    csv_writer.writerow([f"Correy: {percent_votes_correy}% ({votes_correy:,})"])
+    csv_writer.writerow([f"Li: {percent_votes_li}% ({votes_li:,})"])
+    csv_writer.writerow([f"O'Tooley: {percent_votes_otooley}% ({votes_otooley:,})"])
     csv_writer.writerow(["-------------------------"])
     csv_writer.writerow([f"Winner: {winner}"])
     csv_writer.writerow(["-------------------------"])
